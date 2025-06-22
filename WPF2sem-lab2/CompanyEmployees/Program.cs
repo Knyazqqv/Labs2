021c2340403ee8339ad793306b56f03b6d21083f
Knyazqqv <knyasfng@gmail.com>
2025-06-22 19:02:53 +0300

    Initial commit for lab2

---
+++ WPF2sem-lab2/CompanyEmployees/Program.cs
@@ -0,0 +1,19 @@
+using NLog;
+using Start;
+
+namespace CompanyEmployees;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
